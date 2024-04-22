80e6114cfb9e86cee266eb5a54c693cb2baaa8f9
l3miage-ourzikj <Jugurta.Ourzik@etu.univ-grenoble-alpes.fr>
2024-04-22 15:46:55 +0200

    +deliverymanEndpoint: deliveryman endpoint overall tested

--- server/src/test/java/fr/uga/l3miage/integrator/controllers/DeliverymanControllerTest.java
+++ server/src/test/java/fr/uga/l3miage/integrator/controllers/DeliverymanControllerTest.java
@@ -1,16 +1,14 @@
 package fr.uga.l3miage.integrator.controllers;
 
-
 import fr.uga.l3miage.integrator.components.TourComponent;
 import fr.uga.l3miage.integrator.dataTypes.Address;
+import fr.uga.l3miage.integrator.exceptions.NotFoundErrorResponse;
 import fr.uga.l3miage.integrator.exceptions.technical.DayNotFoundException;
 import fr.uga.l3miage.integrator.exceptions.technical.TourNotFoundException;
 import fr.uga.l3miage.integrator.models.*;
 import fr.uga.l3miage.integrator.repositories.*;
 import fr.uga.l3miage.integrator.responses.TourDMResponseDTO;
 import fr.uga.l3miage.integrator.services.TourService;
-import org.hibernate.Hibernate;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,20 +17,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,properties = "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,properties = {"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect","spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true"})
 @AutoConfigureTestDatabase
 @AutoConfigureWebClient
 public class DeliverymanControllerTest {
@@ -58,21 +53,9 @@ public class DeliverymanControllerTest {
     private CustomerRepository customerRepository;
     @SpyBean
     private TourComponent tourComponent;
-    @AfterEach
-    public void clear() {
-        tourRepository.deleteAll();
-        dayRepository.deleteAll();
-        customerRepository.deleteAll();
-        orderRepository.deleteAll();
-        deliveryRepository.deleteAll();
-        employeeRepository.deleteAll();
-        truckRepository.deleteAll();
-        warehouseRepository.deleteAll();
-
-    }
 
 
-    @Transactional
+
     @Test
     void getTourOK() throws TourNotFoundException, DayNotFoundException {
         //given
@@ -103,8 +86,8 @@ public class DeliverymanControllerTest {
         Address customerAddress= Address.builder().address("02 rue des rois").city("paris").postalCode("75012").build();
 
         CustomerEntity customer=CustomerEntity.builder().lastName("axel").firstName("leroy").email("axel@gmail.com").address(customerAddress).build();
-        OrderEntity order1=OrderEntity.builder().reference("c23").customer(customer).build();
         customerRepository.save(customer);
+        OrderEntity order1=OrderEntity.builder().reference("c23").customer(customer).build();
         orderRepository.save(order1);
 
         DeliveryEntity d1=DeliveryEntity.builder().reference("L1").orders(Set.of(order1)).build();
@@ -120,17 +103,66 @@ public class DeliverymanControllerTest {
         day.setTours(tours);
         dayRepository.save(day);
 
-
         //when
         TourDMResponseDTO expectedResponse = tourService.getDeliveryTourOfTheDay(man2.getEmail());
 
         ResponseEntity<TourDMResponseDTO> response = testRestTemplate.exchange("/api/v2.0/deliveryman/tour?email={email}", HttpMethod.GET, new HttpEntity<>(null, headers), TourDMResponseDTO.class, urlParams);
 
         //then
-        //assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(expectedResponse);
+        verify(tourComponent, times(2)).getTourOfTheDay(anyString());
+        verify(tourService, times(2)).getDeliveryTourOfTheDay(anyString());
+
+
+    }
+
+
+    @Test
+    void getTourNotFoundBecauseOfNotFoundDay() throws TourNotFoundException, DayNotFoundException {
+        //given
+        final HttpHeaders headers = new HttpHeaders();
+
+        final Map<String, Object> urlParams = new HashMap<>();
+        urlParams.put("email", "hola@gmail.com");
+
+        //when
+        NotFoundErrorResponse expectedResponse = NotFoundErrorResponse.builder().uri("/api/v2.0/deliveryman/tour").errorMessage("No day was planned for today : "+LocalDate.now()).build();
+
+        ResponseEntity<NotFoundErrorResponse> response = testRestTemplate.exchange("/api/v2.0/deliveryman/tour?email={email}", HttpMethod.GET, new HttpEntity<>(null, headers), NotFoundErrorResponse.class, urlParams);
+
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(expectedResponse);
         verify(tourComponent, times(1)).getTourOfTheDay(anyString());
         verify(tourService, times(1)).getDeliveryTourOfTheDay(anyString());
+
+
+    }
+
+    @Test
+    void getTourNotFoundBecauseOfNotFoundDeliveryman() throws TourNotFoundException, DayNotFoundException {
+        //given
+        final HttpHeaders headers = new HttpHeaders();
+
+        final Map<String, Object> urlParams = new HashMap<>();
+        urlParams.put("email", "hola@gmail.com");
+
+        //create and save a planned day including the above tour
+        DayEntity day = DayEntity.builder().reference("J123G").build();
+        day.setDate(LocalDate.now());
+        dayRepository.save(day);
+
+        //when
+        NotFoundErrorResponse expectedResponse = NotFoundErrorResponse.builder().uri("/api/v2.0/deliveryman/tour").errorMessage("No tour was found for <hola@gmail.com>").build();
+        ResponseEntity<NotFoundErrorResponse> response = testRestTemplate.exchange("/api/v2.0/deliveryman/tour?email={email}", HttpMethod.GET, new HttpEntity<>(null, headers), NotFoundErrorResponse.class, urlParams);
+
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).usingRecursiveComparison().isEqualTo(expectedResponse);
+        verify(tourComponent, times(1)).getTourOfTheDay(anyString());
+        verify(tourService, times(1)).getDeliveryTourOfTheDay(anyString());
+
 
     }
 }
