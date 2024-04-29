41ecf0bba260a7336f82c686201fbeb9aacf29f2
l3miage-ourzikj <Jugurta.Ourzik@etu.univ-grenoble-alpes.fr>
2024-04-29 10:16:46 +0200

    +dev: added customer address to bundle

---
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/dataTypes/MultipleOrder.java
@@ -0,0 +1,22 @@
+package fr.uga.l3miage.integrator.dataTypes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import java.util.Set;
+
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@SuperBuilder
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MultipleOrder {
+    private Set<String> orders;
+    private String address;
+}
