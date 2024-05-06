24d1ed478fc56a2edf4bbfc78a3527cd71ea830c
l3miage-monachov <Vivian.Monachon@etu.univ-grenoble-alpes.fr>
2024-05-06 10:40:35 +0200

    finitions

--- rest-api/src/main/java/fr/uga/l3miage/integrator/responses/SetUpBundleResponse.java
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/responses/SetUpBundleResponse.java
@@ -1,8 +1,6 @@
 package fr.uga.l3miage.integrator.responses;
 
-import fr.uga.l3miage.integrator.dataTypes.MultipleOrder;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Data;
 
 import java.util.Set;
